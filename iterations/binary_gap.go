08da23901ed6ee192dd2a53cc97b370edba766b4
Huong Nguyen <huong.nguyen2@iqvia.com>
2024-08-28 14:07:14 +0700

    iterations: BinaryGap

--- iterations/binary_gap.go
+++ iterations/binary_gap.go
@@ -1,6 +1,30 @@
 package iterations
 
+import (
+	"strconv"
+)
 
 func BinaryGap(N int) int {
-	return 0
+	binStr := strconv.FormatInt(int64(N), 2)
+
+	counter := 0
+	maxCount := 0
+	started := false
+
+	for _, c := range binStr {
+		if c == '1' {
+			if started && counter > maxCount {
+				maxCount = counter
+			}
+
+			counter = 0
+			started = true
+		}
+
+		if c == '0' {
+			counter++
+		}
+	}
+
+	return maxCount
 }
--- iterations/binary_gap_test.go
+++ iterations/binary_gap_test.go
@@ -1,31 +1,28 @@
 package iterations
 
-
 import (
 	"testing"
-	"vnnvanhuong/codility/iterations"
 )
-
 
 func TestBinaryGap(t *testing.T) {
 
-	if iterations.BinaryGap(9) != 2 {
-		t.Errorf("Expected: 2, Got: %d", iterations.BinaryGap(9))
+	if r := BinaryGap(9); r != 2 {
+		t.Errorf("Expected: 2, Got: %d", r)
 	}
 
-	if iterations.BinaryGap(529) != 4 {
-		t.Errorf("Expected: 4, Got: %d", iterations.BinaryGap(529))
+	if r := BinaryGap(529); r != 4 {
+		t.Errorf("Expected: 4, Got: %d", r)
 	}
 
-	if iterations.BinaryGap(20) != 1 {
-		t.Errorf("Expected: 1, Got: %d", iterations.BinaryGap(20))
+	if r := BinaryGap(20); r != 1 {
+		t.Errorf("Expected: 1, Got: %d", r)
 	}
 
-	if iterations.BinaryGap(15) != 0 {
-		t.Errorf("Expected: 0, Got: %d", iterations.BinaryGap(15))
+	if r := BinaryGap(15); r != 0 {
+		t.Errorf("Expected: 0, Got: %d", r)
 	}
 
-	if iterations.BinaryGap(32) != 0 {
-		t.Errorf("Expected: 0, Got: %d", iterations.BinaryGap(32))
+	if r := BinaryGap(32); r != 0 {
+		t.Errorf("Expected: 0, Got: %d", r)
 	}
 }
