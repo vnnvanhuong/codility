05f3836edb475dd3e2d467508b79d22d3b7abdf9
Huong Nguyen <vnnvanhuong@gmail.com>
2024-10-15 21:35:03 +0700

    prefix sums: count div O(1)

--- prefixsums/count_div.go
+++ prefixsums/count_div.go
@@ -3,13 +3,28 @@ package prefixsums
 // O(N)
 // loop through from A to B, perform the division and count
 
-func CountDiv(A, B, K int) int {
-	count := 0
-	for i := A ; i <= B; i++ {
-		if i % K == 0 {
-			count++
-		}
-	}
+// O(1)
+// the first divisable ceil(A/K)
+// remaining divisable  floor(B - ceil(A/K)*K)
 
-	return count
+// 0 1 2 3 4 5 6 7 8 9 10 11
+// 6
+// 10
+// (10 - 6)/2 = 4
+// return 4 + 1
+
+func CountDiv(A, B, K int) int {
+	// first div
+    if A % K != 0 {
+        A = (A - A%K) + K
+    }
+
+    // last div
+    if B % K != 0 {
+        B = B - B%K
+    }
+
+    midDivNums := (B - A)/K
+
+    return midDivNums + 1 // first div
 }
--- prefixsums/count_div_test.go
+++ prefixsums/count_div_test.go
@@ -6,4 +6,8 @@ func TestCountDiv(t *testing.T) {
 	if r := CountDiv(6,11,2); r != 3 {
 		t.Errorf("Expected: 3, Got: %d", r)
 	}
+
+	if r := CountDiv(6,11,3); r != 2 {
+		t.Errorf("Expected: 2, Got: %d", r)
+	}
 }
