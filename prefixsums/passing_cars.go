aeda69f95ec045f84555cf785e921bb3b72064a8
Huong Nguyen <vnnvanhuong@gmail.com>
2024-10-15 09:21:11 +0700

    prefix sums: passing cars O(N)

--- prefixsums/passing_cars.go
+++ prefixsums/passing_cars.go
@@ -13,16 +13,26 @@ package prefixsums
 // loop through element
 // if i == 0, loop through remaining elements and acount all j == 1
 
+// O(N)
+// We need to remember the number of car travelling east
+// We increase the count when meeting a care travelling west
+// 0 1 0 1 1
+// e = 1, c = 0
+// e = 1, c = 1
+// e = 2, c = 1
+// e = 2, c = 3
+// e = 2, c = 5
+
 func PassingCars(A []int) int {
 	count := 0
-	for i := 0; i < len(A) - 1 ; i++ {
+	eastCars := 0
+	for i := 0; i < len(A) ; i++ {
 		if A[i] == 0 {
-			for j := i + 1; j < len(A); j++ {
-				if A[j] == 1 {
-					count++
-				}
-			}
-		} 
+			eastCars++
+			continue
+		}
+
+		count += eastCars
 	}
 
 	if count > 1000000000 {
