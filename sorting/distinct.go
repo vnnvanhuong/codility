b4d7a4bf771990b4c7719adb367090a5780d51be
Huong Nguyen <vnnvanhuong@gmail.com>
2024-10-22 09:07:47 +0700

    sorting: distinct O(N) or O(N*log(N))

--- sorting/distinct.go
+++ sorting/distinct.go
@@ -1,18 +1,32 @@
 package sorting
 
+import "sort"
+
 // Brute Force O(N**2)
 // Create an array to contain unique elements
 // return size of the array
 
 // HashMap O(N*log(N)) or O(N)
-func Distinct(A []int) int {
-	result := make(map[int]bool)
 
-	for i := range A {
-		if !result[A[i]] {
-			result[A[i]]=true
-		}
+// Sorting O(N)
+func Distinct(A []int) int {
+	if len(A) == 0 {
+		return 0
 	}
 
-	return len(result)
+	sort.Ints(A)
+
+	current := A[0]
+	count := 1
+
+	for i := 1; i < len(A); i++ {
+		if current == A[i] {
+			continue
+		}
+
+		current = A[i]
+		count++
+	}
+
+	return count
 }
--- sorting/distinct_test.go
+++ sorting/distinct_test.go
@@ -6,11 +6,15 @@ import (
 )
 
 func TestDistinct(t *testing.T) {
-	if r := sorting.Distinct([]int{2,1,1,2,3,1}); r != 3 {
+	if r := sorting.Distinct([]int{2, 1, 1, 2, 3, 1}); r != 3 {
 		t.Errorf("Expected: 3, Got: %d", r)
 	}
 
 	if r := sorting.Distinct([]int{1}); r != 1 {
 		t.Errorf("Expected: 1, Got: %d", r)
-	} 
+	}
+
+	if r := sorting.Distinct([]int{}); r != 0 {
+		t.Errorf("Expected: 0, Got: %d", r)
+	}
 }
