cf31c05035aef7a5a88462539b5978ea0e23788b
Huong Nguyen <vnnvanhuong@gmail.com>
2024-10-23 13:27:23 +0700

    sorting: number of disc intersections (brute force)

--- sorting/number_of_disc_intersections.go
+++ sorting/number_of_disc_intersections.go
@@ -1,8 +1,55 @@
 package sorting
 
-// 1 5 2 1 4 0
+import (
+	"fmt"
+)
+
+// Approach 1: Brute Force
 func NumberOfDiscIntersections(A []int) int {
-	result := 0
+	n := len(A)
+	slices := make([][]int, n)
+
+	for i := range n {
+		slices[i] = make([]int, 2)
+	}
+
+	for i := range A {
+		slices[i][0] = i - A[i]
+		slices[i][1] = i + A[i]
+	}
+
+	counter := make(map[string]int, n)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if j == i {
+				continue
+			}
+
+			isIntersect := false
+			for p := slices[i][0]; p <= slices[i][1]; p++ {
+				for q := slices[j][0]; q <= slices[j][1]; q++ {
+					if p == q {
+						isIntersect = true
+						pair := fmt.Sprintf("%d_%d", i, j)
+						revertPair := fmt.Sprintf("%d_%d", j, i)
+						if counter[pair] == 0 &&
+							counter[revertPair] == 0 {
+							counter[pair]++
+						}
+						break
+					}
+				}
+
+				if isIntersect {
+					break
+				}
+			}
+
+		}
+	}
+
+	result := len(counter)
 
 	if result > 10000000 {
 		return -1
--- sorting/number_of_disc_intersections_test.go
+++ sorting/number_of_disc_intersections_test.go
@@ -1,12 +1,22 @@
 package sorting_test
 
 import (
+	"math"
 	"testing"
+
 	"vnnvanhuong/codility/sorting"
 )
 
 func TestNumberOfDiscIntersections(t *testing.T) {
 	if r := sorting.NumberOfDiscIntersections([]int{1, 5, 2, 1, 4, 0}); r != 11 {
 		t.Errorf("Expected: 11, Got: %d", r)
+	}
+
+	if r := sorting.NumberOfDiscIntersections([]int{}); r != 0 {
+		t.Errorf("Expected: 0, Got: %d", r)
+	}
+
+	if r := sorting.NumberOfDiscIntersections([]int{math.MaxInt32, math.MaxInt32}); r != 1 {
+		t.Errorf("Expected: 1, Got: %d", r)
 	}
 }
