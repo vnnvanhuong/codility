4b837b1757ecf5374ef2f2c6fcb16a4616592b8d
Huong Nguyen <vnnvanhuong@gmail.com>
2024-10-24 09:00:06 +0700

    sorting: number of disc intersections O(N*log(N)) or O(N)

--- sorting/number_of_disc_intersections.go
+++ sorting/number_of_disc_intersections.go
@@ -1,59 +1,74 @@
 package sorting
 
-import (
-	"fmt"
-)
-
 // Approach 1: Brute Force
+
+// Approach 2: Counting and make the array asc in order
+// 1, 5, 2, 1, 4, 0
+// slices   [[0 1] [0 5] [0 4] [2 4] [0 5] [5 5]]
+// 0______1______2______3______4______5
+// 0______1
+// 0__________________________________5
+// 0___________________________4
+//               2_____________4
+// 0__________________________________5
+//                                    5
+// starts   [4 0 1 0 0 1]
+//
+// sorting by accumulate the previous state
+// startCounters [4 4 5 5 5 6]
+// at position x there is y starting state so far
+// at position 0 there is 4 starting state so far
+// at position 1 there is 4 starting state so far
+// ...
+// count the intersections
+// position x has at least startCounters[x] intersections - (itseft + previous starting states)
+// or more if x has end with more starting states
+//
+// 0 - max(4, 4) - 1 = 3
+// 1 - max(4, 6) - 2 = 4
+// 2 - max(5, 6) - 3 = 3
+// 3 - max(5, 5) - 4 = 1
+// 4 - max(5, 6) - 5 = 1
+// 5 - max(6, 5) - 6 = 0
 func NumberOfDiscIntersections(A []int) int {
 	n := len(A)
-	slices := make([][]int, n)
-
-	for i := range n {
-		slices[i] = make([]int, 2)
-	}
-
-	for i := range A {
-		slices[i][0] = i - A[i]
-		slices[i][1] = i + A[i]
-	}
-
-	counter := make(map[string]int, n)
+	starts := make([]int, n)
 
 	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if j == i {
-				continue
-			}
+		radius := A[i]
+		startPos := i - radius
+		if startPos < 0 {
+			startPos = 0
+		}
 
-			isIntersect := false
-			for p := slices[i][0]; p <= slices[i][1]; p++ {
-				for q := slices[j][0]; q <= slices[j][1]; q++ {
-					if p == q {
-						isIntersect = true
-						pair := fmt.Sprintf("%d_%d", i, j)
-						revertPair := fmt.Sprintf("%d_%d", j, i)
-						if counter[pair] == 0 &&
-							counter[revertPair] == 0 {
-							counter[pair]++
-						}
-						break
-					}
-				}
+		starts[startPos]++
+	}
 
-				if isIntersect {
-					break
-				}
-			}
+	total := 0
+	for i := 0; i < n; i++ {
+		total += starts[i]
+		starts[i] = total
+	}
 
+	interSections := 0
+	for i := 0; i < n; i++ {
+		radius := A[i]
+		endPos := i + radius
+		if endPos > n-1 {
+			endPos = n - 1
+		}
+
+		m := starts[i]
+		if starts[i] < starts[endPos] {
+			m = starts[endPos]
+		}
+
+		interSections += m - (i + 1)
+
+		if interSections > 10000000 {
+			return -1
 		}
 	}
 
-	result := len(counter)
-
-	if result > 10000000 {
-		return -1
-	}
-
-	return result
+	return interSections
 }
