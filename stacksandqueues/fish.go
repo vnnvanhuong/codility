54e0724206ee323318920e051c245c82cfecf5da
Huong Nguyen <vnnvanhuong@gmail.com>
2024-10-29 10:01:09 +0700

    stacks and queues: fish

---
+++ stacksandqueues/fish.go
@@ -0,0 +1,6 @@
+package stacksandqueues
+
+// 4:0 3:1 2:0 1:0 5:0
+func Fish(A, B []int) int {
+	return 0
+}
---
+++ stacksandqueues/fish_test.go
@@ -0,0 +1,31 @@
+package stacksandqueues_test
+
+import (
+	"testing"
+	"vnnvanhuong/codility/stacksandqueues"
+)
+
+func TestFish(t *testing.T) {
+	tcs := []struct {
+		name      string
+		size      []int
+		direction []int
+		expected  int
+	}{
+		{
+			name:      "test case 1",
+			size:      []int{4, 3, 2, 1, 5},
+			direction: []int{0, 1, 0, 0, 0},
+			expected:  2,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := stacksandqueues.Fish(tc.size, tc.direction)
+			if actual != tc.expected {
+				t.Errorf("Expected: %d, Got: %d", tc.expected, actual)
+			}
+		})
+	}
+}
