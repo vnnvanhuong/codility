2fcd922ea6893b170cf2c2c29815c6b0ae74dfd1
Huong Nguyen <vnnvanhuong@gmail.com>
2024-11-06 09:52:21 +0700

    max slice problem: profit

--- maximumsliceproblem/max_profit.go
+++ maximumsliceproblem/max_profit.go
@@ -1,29 +1,14 @@
 package maximumsliceproblem
 
-// Take profit each day and keep track the max
-// The profit can be cumulative
-// 23171, 21011, 21123, 21366, 21013, 21367
-//
-// profit=-2160, max=0, cumulative=0
-// profit=112, max=112, cummlative=112
-// profit=243, max=243, cummlative=112+243
+// Brute Force
+// 0 1 2 3 4 5
+// p=0, m=0
+
 func MaxProfit(A []int) int {
-	maxProfit := 0
-	accProfit := 0
-	profit := 0
-
-	for i := 1; i < len(A); i++ {
-		profit = A[i] - A[i-1]
-		if profit < 0 {
-			accProfit = 0
-			continue
-		}
-
-		accProfit += profit
-		if accProfit > maxProfit {
-			maxProfit = accProfit
-		}
+	var maxProfit, profit int
+	for i := 0; i < len(A)-1; i++ {
+		profit = max(0, profit+A[i+1]-A[i])
+		maxProfit = max(maxProfit, profit)
 	}
-
 	return maxProfit
 }
