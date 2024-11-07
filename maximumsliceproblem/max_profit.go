2f03df733766296269886fcd472d75fcdffed479
Huong Nguyen <vnnvanhuong@gmail.com>
2024-11-07 08:22:00 +0700

    maximum slice problem: max profit

--- maximumsliceproblem/max_profit.go
+++ maximumsliceproblem/max_profit.go
@@ -1,14 +1,29 @@
 package maximumsliceproblem
 
-// Brute Force
-// 0 1 2 3 4 5
-// p=0, m=0
+// There is only one selling action
+// Hence, we either
+// - sell a stock immediately in next day
+// - or, holder a stock a sell in some next days
+// for each stock price
+// check the profit: price[next] - price[today]
+// if profit > 0 means it can be a candidate for maxProfit
+// if profit < 0 means we won't sell the stock, keep holding (profit still = 0)
 
 func MaxProfit(A []int) int {
-	var maxProfit, profit int
+	maxProfit := 0
+	profit := 0
 	for i := 0; i < len(A)-1; i++ {
-		profit = max(0, profit+A[i+1]-A[i])
-		maxProfit = max(maxProfit, profit)
+		profit += A[i+1] - A[i]
+
+		if profit < 0 {
+			profit = 0
+		}
+
+		if profit > maxProfit {
+			maxProfit = profit
+		}
+
 	}
+
 	return maxProfit
 }
