e1868b05476fba09a478e23d46b744ec9e4bc894
Huong Nguyen <vnnvanhuong@gmail.com>
2024-11-13 10:11:45 +0700

    prime number: flags (first attempt)

--- primenumber/flags.go
+++ primenumber/flags.go
@@ -1,5 +1,38 @@
 package primenumber
 
 func Flags(A []int) int {
-	return 0
+	n := len(A)
+	if n < 3 {
+		return 0
+	}
+
+	maxPeaks := len(A) / 3
+	maxFlags := 0
+
+	for i := maxPeaks; i > 0; i-- {
+		flags := 0
+		previousPeak := 0
+		for j := 1; j < n-1; j++ {
+			if A[j] > A[j-1] && A[j] > A[j+1] {
+				if j == 1 || j-previousPeak >= i {
+					flags++
+					previousPeak = j
+				}
+			}
+		}
+
+		maxFlags = max(maxFlags, flags)
+
+		if maxFlags == i {
+			break
+		}
+	}
+
+	return maxFlags
 }
+
+// brute force to find the maximum flags
+// i from 1 to len(A)/3 since maxium peaks if len(A)/3
+// j from 0 to len(A)
+// max = i if there i peaks
+// return max
