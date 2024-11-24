8b16a5276225b372ddc02092b68d3b0cdc274e1e
Huong Nguyen <vnnvanhuong@gmail.com>
2024-11-24 19:21:41 +0700

    sieve of eratosthenes: count semiprimes

--- sieveoferatosthenes/count_semiprimes.go
+++ sieveoferatosthenes/count_semiprimes.go
@@ -1,56 +1,47 @@
 package sieveoferatosthenes
 
-// understand the problem
-// 26
-// 1:26
-// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
-// 4, 6, 9, 10, 14, 15, 21, 22, 25, 26
-// 4:10
-// 4 5 6 7 8 9 10
-// 4 6 9 10
-// 16:20
-// 16 17 18 19 20
-// 0
-// brute force
-// find primes O(NLogN) with sieve of eratosthenes
-// find semiprimes O(N*N) loop through primes till the product of two < N
-// query O(N*M)
+// find primes [1..N]
+// find semiprimes [1..N]
+// prefixSum
+// answer the query
 func CountSemiPrimes(N int, P []int, Q []int) []int {
-	composite := make([]int, N+1)
-	composite[0] = 1
-	composite[1] = 1
+	composites := make([]bool, N+1)
+	composites[0] = true
+	composites[1] = true
 	for i := 2; i*i <= N; i++ {
-		if composite[i] == 0 {
-			for k := i * i; k <= N; k += i {
-				composite[k] = 1
+		for j := i * i; j <= N; j += i {
+			if !composites[i] {
+				composites[j] = true
 			}
 		}
 	}
 
-	primes := []int{}
-	for k := range composite {
-		if composite[k] == 0 {
-			primes = append(primes, k)
+	semiprimes := make([]bool, N+1)
+	for i := 2; i*i <= N; i++ {
+		if !composites[i] {
+			semiprimes[i*i] = true
+
+			for j := i * i; j <= N; j += i {
+				if !composites[j/i] {
+					semiprimes[j] = true
+				}
+			}
 		}
+	}
+
+	prefixsums := make([]int, N+1)
+	for i := 1; i <= N; i++ {
+		if semiprimes[i] {
+			prefixsums[i] = prefixsums[i-1] + 1
+			continue
+		}
+
+		prefixsums[i] = prefixsums[i-1]
 	}
 
 	result := make([]int, len(P))
 	for i := 0; i < len(P); i++ {
-		count := 0
-		x, y := 0, 0
-		for x < len(primes) && primes[x]*primes[x] <= Q[i] {
-			s := primes[x] * primes[y]
-			if s < P[i] || s > Q[i] || y == len(primes) {
-				x++
-				y = x
-				continue
-			}
-
-			count++
-			y++
-		}
-
-		result[i] = count
+		result[i] = prefixsums[Q[i]] - prefixsums[P[i]-1]
 	}
 
 	return result
